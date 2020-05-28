a948edb40d8c8fb2d7f4b7ee98992b115ff50121
PSingh <62577068+parampuneetsingh@users.noreply.github.com>
2020-05-28 12:49:37 -0400

    Update IsGraphBipartite_785.java

--- IsGraphBipartite_785.java
+++ IsGraphBipartite_785.java
@@ -3,8 +3,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 //https://leetcode.com/problems/is-graph-bipartite/
-//https://www.youtube.com/watch?v=FofydiwP5YQ
+//https://www.youtube.com/watch?v=Gmp51E8HVVs
+//https://www.youtube.com/watch?v=hWFqtlbnQV8
 
+//Similar to Leetcode Problem 886
 public class IsGraphBipartite_785 {
 
 	private boolean bfs(int node, int[][] graph, int[] color) {
@@ -35,10 +37,37 @@ public class IsGraphBipartite_785 {
 		return true;
 	}
 
+	// DFS
+	public boolean isBipartiteDFS(int[][] graph) {
+		int nodes = graph.length;
+		int[] colors = new int[nodes];
+
+		for (int i = 0; i < graph.length; i++)
+			if ((colors[i] == 0) && !dfs(graph, colors, 1, i)) {
+				return false;
+			}
+
+		return true;
+	}
+
+	private boolean dfs(int[][] graph, int[] colors, int color, int node) {
+		if (colors[node] != 0) {
+			return colors[node] == color;
+		}
+
+		colors[node] = color;
+		for (int n : graph[node]) {
+			if (!dfs(graph, colors, -color, n)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		IsGraphBipartite_785 bipartite = new IsGraphBipartite_785();
 		int[][] graph = { { 1, 3 }, { 0, 2 }, { 1, 3 }, { 0, 2 } };
-		System.out.println(bipartite.isBipartiteBFS(graph));
+		System.out.println(bipartite.isBipartiteDFS(graph));
 	}
 
 }
