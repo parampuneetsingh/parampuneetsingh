5213d150053f9a623e9df87699b8caae43358031
PSingh <62577068+parampuneetsingh@users.noreply.github.com>
2020-05-28 12:48:14 -0400

    Update LongestSubstring_3.java

--- LongestSubstring_3.java
+++ LongestSubstring_3.java
@@ -5,25 +5,33 @@ Given "pwwkew", the answer is "wke", with the length of 3.
 Note that the answer must be a substring, "pwke" is a subsequence and not a substring.*/
 
 public class LongestSubstring_3 {
-	
-	public int lengthOfLongestSubstring2(String s) {
-		int[] mOccur = new int[256];
-        int maxL = 0;
-        for(int i = 0, j = 0; i < s.length(); ++i){
-            char ch = s.charAt(i);
-            ++mOccur[ch];
-            while(mOccur[ch] > 1){
-                --mOccur[s.charAt(j++)];
-            }
-            maxL = Math.max(maxL, i - j + 1);
-        }
-        return maxL;
-    }
+
+	public int lengthOfLongestSubstring(String s) {
+		int left = 0, right = 0;
+		int max = 0;
+
+		int[] charAry = new int[128];
+
+		while (right < s.length()) {
+			while (right < s.length() && charAry[s.charAt(right)] == 0) {
+				charAry[s.charAt(right++)]--;
+			}
+
+			max = Math.max(max, right - left);
+			while (left < right && right < s.length() && charAry[s.charAt(right)] < 0) {
+				charAry[s.charAt(left++)]++;
+			}
+		}
+
+		return max;
+	}
 
 	public static void main(String[] args) {
-		String str = "abcabcbb";
+		// String str = "abcabcbb";
+		// String str = "pwwkew";
+		String str = "dvdf";
 		LongestSubstring_3 lols = new LongestSubstring_3();
-		System.out.println(lols.lengthOfLongestSubstring2(str)); //Solution from Leetcode
+		System.out.println(lols.lengthOfLongestSubstring(str)); // Solution from Leetcode
 	}
 
 }
